c8dcde0b7c1e6ffd349701dc10a272451725e600
play-apps <appbit.in@gmail.com>
2024-07-21 19:46:15 +0530

    Add files via upload

---
+++ data/js/load.js
@@ -0,0 +1,12 @@
+
+	
+	document.getElementById("footer").innerHTML =
+		"<div class='bottom'><a class='link' href='https://forms.gle/4N93UqhVyjwoPAqd6'>Login</a> | <a class='link' href='#'>Donate</a> | <a class='link' href='https://forms.gle/3K4SPBr6ZJJgpheJA'>Contact</a></div>"
+	+ 	"<h3 class='made_by'>Made with <i class='fa-regular fa-heart'></i> Ads Free &copy; 2024</h3>"	
+	;
+
+$(function(){
+  $("#header").load("header.html"); 
+});
+	
+	
