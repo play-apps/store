df99df7a04b3cdcfc0923b4eaad357060f80453e
play-apps <appbit.in@gmail.com>
2024-07-21 21:49:30 +0530

    Update load2.js

--- data/js/load2.js
+++ data/js/load2.js
@@ -3,6 +3,7 @@
 	+ 	"<h3 class='made_by'>Made with <i class='fa-regular fa-heart'></i> Ads Free &copy; 2024</h3>"	
 	;
 
+
 $(function(){
   $("#header").load("/store/header.html");
   $("#all").load("all.html");   
