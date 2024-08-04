744e316f0fcc3e114b0711b3efec650a1daf8524
play-apps <appbit.in@gmail.com>
2024-08-04 19:40:36 +0530

    Update load.js

--- data/js/load.js
+++ data/js/load.js
@@ -1,5 +1,5 @@
 document.getElementById("footer").innerHTML =
-		"<div class='bottom'><a class='link' href='https://forms.gle/4N93UqhVyjwoPAqd6'>Login</a> | <a class='link' href='#'>Donate</a> | <a class='link' href='https://forms.gle/3K4SPBr6ZJJgpheJA'>Contact</a></div>"
+		"<div class='bottom'><a class='link' href='https://forms.gle/4N93UqhVyjwoPAqd6'>Login</a> | <a class='link' href='/store/link/donate'>Donate</a> | <a class='link' href='https://forms.gle/3K4SPBr6ZJJgpheJA'>Contact</a></div>"
 	+ 	"<h3 class='made_by'>Made with <i class='fa-regular fa-heart'></i> Ads Free &copy; 2024</h3>"	
 	;
 
